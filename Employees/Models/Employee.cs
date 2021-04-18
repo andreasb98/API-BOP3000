7a57958a4295bb857cafb3694c29411b69daf828
eindreis <46956058+eindreis@users.noreply.github.com>
2021-04-18 14:07:37 +0200

    Add files via upload

---
+++ Employees/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string EmploymentDate { get; set; }
+    }
+}
